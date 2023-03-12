59d94e82e01ff904759bd25b0b12b74b18622bf2
NicoJC25 <nejuez@misena.edu.co>
2023-03-12 15:25:18 -0500

    Pagina web

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos/estilos.css">
+    <title>Hotel SENA</title>
+</head>
+<body>
+    <header>
+        <div class="cab_logo">
+            <div class="caj_logo">
+                <img src="imgs/Logo Sena Blanco.png" alt="cab_logo">
+            </div>
+        </div>
+        <div class="cab_InicioS">
+            <p>Iniciar Sesion</p>
+        </div>
+        <div class="cab_Registro">
+            <p>Registrarse</p>
+        </div>
+        <div class="cab_Ayuda">
+            <p>?</p>
+        </div>
+        <section>
+            <div class="caja_cal">
+                <div class="caja_entrada">
+                    <p>Entrada</p>
+                </div>
+            </div>
+        </section>
+</body>
+</html>
